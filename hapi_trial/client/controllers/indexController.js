dd30c58bd3d8b01f4c2ebcdd3798c8e07f2ecc7f
Nicole Rey Arriesga <nicolereya@gmail.com>
2016-07-21 10:05:34 +0800

    used HTML-CSS-JS Prettify to format :)

--- hapi_trial/client/controllers/indexController.js
+++ hapi_trial/client/controllers/indexController.js
@@ -1,38 +1,38 @@
-firstApp.controller('indexController', ['$scope', '$http', 'ServerURLService', function($scope, $http, ServerURLService){
+firstApp.controller('indexController', ['$scope', '$http', 'ServerURLService', function($scope, $http, ServerURLService) {
     $scope.server_response = [];
 
-    $scope.AddNameToServer = function(){
-    	console.log("Add name to SERVER");
+    $scope.AddNameToServer = function() {
+        console.log("Add name to SERVER");
 
-    	$http.post(ServerURLService.url('addName'), {name: $scope.name})
-    		.success(function(response){
-    			$scope.name = '';
-	    		$scope.RequestToServer();
-	    	});
-    };
-
-    $scope.SaveNameToServer = function(id, u_name){
-        console.log("Save name to SERVER");
-
-        $http.post(ServerURLService.url('saveName'), {_id: id, name: u_name})
-            .success(function(response){
+        $http.post(ServerURLService.url('addName'), { name: $scope.name })
+            .success(function(response) {
+                $scope.name = '';
                 $scope.RequestToServer();
             });
     };
 
-    $scope.DeleteNameFromServer = function(id){
-    	console.log("Delete name from SERVER");
+    $scope.SaveNameToServer = function(id, u_name) {
+        console.log("Save name to SERVER");
 
-    	$http.post(ServerURLService.url('deleteName'), {_id: id})
-    		.success(function(response){
-    			$scope.RequestToServer();
-    		});
+        $http.post(ServerURLService.url('saveName'), { _id: id, name: u_name })
+            .success(function(response) {
+                $scope.RequestToServer();
+            });
     };
 
-    $scope.RequestToServer = function(){
+    $scope.DeleteNameFromServer = function(id) {
+        console.log("Delete name from SERVER");
+
+        $http.post(ServerURLService.url('deleteName'), { _id: id })
+            .success(function(response) {
+                $scope.RequestToServer();
+            });
+    };
+
+    $scope.RequestToServer = function() {
         console.log("Request names from SERVER");
 
-        $http.get(ServerURLService.url('getAllData')).success(function(response){
+        $http.get(ServerURLService.url('getAllData')).success(function(response) {
             $scope.server_response = response;
         });
     };
