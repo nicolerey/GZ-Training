03de2649643856e660e2b4a7f0110db43d697490
Nicole Rey Arriesga <nicolereya@gmail.com>
2016-07-21 10:54:53 +0800

    fixed a stupid mistake :)

--- hapi_trial/db.js
+++ hapi_trial/db.js
@@ -1,4 +1,4 @@
-exports.DBModule = function(cllbck){
+exports.DBModule = function(cllbck) {
     var MongoClient = require('mongodb').MongoClient;
     var assert_module = require('assert');
     var ObjectID = require('mongodb').ObjectID;
@@ -10,87 +10,87 @@ exports.DBModule = function(cllbck){
     var _collection_name = '';
     var _collection_data = [];
 
-    MongoClient.connect(url, function(err, db){
+    MongoClient.connect(url, function(err, db) {
         assert_module.equal(null, err);
         _db_connection = db;
         cllbck();
     });
 
     var db_data = {
-        setCollectionName: function(name){
+        setCollectionName: function(name) {
             _collection_name = name;
         },
-        closeDBConnection: function(){
+        closeDBConnection: function() {
             _db_connection.close();
         },
-        convertStrToHexStr: function(st){
+        convertStrToHexStr: function(st) {
             return ObjectID(st);
         },
-        searchDB: function(type, callback, data, sort){
+        searchDB: function(type, callback, data, sort) {
             data = (typeof data !== 'undefined') ? data : {};
             sort = (typeof sort !== 'undefined') ? sort : {};
 
             var col = _db_connection.collection(_collection_name);
 
             var fnd = {};
-            if(type=='ALL')
+            if (type == 'ALL')
                 fnd = col.find();
             else
                 fnd = col.find(data).toArray();
 
-            if(sort)
+            if (sort)
                 fnd = fnd.sort(sort);
 
             console.log("Fetched data from " + _collection_name);
 
-            fnd.each(function(err, doc){
+            fnd.each(function(err, doc) {
                 assert_module.equal(err, null);
-                if(doc!==null)
+                if (doc !== null)
                     _collection_data.push(doc);
-                else{
+                else {
                     var c_data = _collection_data;
                     _collection_data = [];
                     callback(c_data);
                 }
             });
         },
-        insertToDB: function(type, data, callback){
+        insertToDB: function(type, data, callback) {
             var col = _db_connection.collection(_collection_name);
 
-            if(type=='ONE')
+            if (type == 'ONE')
                 col.insertOne(data, insertCallback);
-            else if(type=='MANY')
+            else if (type == 'MANY')
                 col.insertMany(data, insertCallback);
 
-            function insertCallback(error, result){
+            function insertCallback(error, result) {
                 assert_module.equal(error, null);
                 console.log("Inserted one row of data to " + _collection_name + ".");
                 callback(result);
             }
         },
-        updateDB: function(type, condition, data, callback){
+        updateDB: function(type, condition, data, callback) {
             var col = _db_connection.collection(_collection_name);
 
-            if(type=='ONE')
+            if (type == 'ONE')
                 col.updateOne(condition, data, updateCallback);
-            else if(type=='MANY')
+            else if (type == 'MANY')
                 col.updateMany(condition, data, updateCallback);
 
-            function updateCallback(error, result){
+            function updateCallback(error, result) {
                 assert_module.equal(error, null);
                 console.log("Updated " + result + " entries in " + _collection_name + ".");
                 callback(result);
             }
         },
-        deleteFromDB: function(type, condition, callback){
+        deleteFromDB: function(type, condition, callback) {
             var col = _db_connection.collection(_collection_name);
 
-            if(type=='ONE')
+            if (type == 'ONE')
                 col.deleteOne(condition, deleteCallback);
-            else if(type=='MANY')
+            else if (type == 'MANY')
                 col.deleteMany(condition, deleteCallback);
 
-            function deleteCallback(error, result){
+            function deleteCallback(error, result) {
                 console.log("Deleted " + type + " from " + _collection_name + ".");
                 callback(result);
             }
--- hapi_trial/index.js
+++ hapi_trial/index.js
@@ -5,120 +5,115 @@ var db_module = require('./db');
 
 var server = new Hapi.Server();
 server.connection({
-	host: 'localhost',
-	port: 8000
+    host: 'localhost',
+    port: 8000
 });
 
-server.register(require('inert'), function(err){
-	if(err)
-		throw err;
+server.register(require('inert'), function(err) {
+    if (err)
+        throw err;
 
-	var db = db_module.DBModule(function(){
-		var data = {};
-		var condition = {};
+    // Static contents
+    server.route([
+    {
+	    method: 'GET',
+	    path: '/',
+	    handler: function(request, reply) {
+	        reply.file('client/views/index.html');
+	    }
+	}, {
+	    method: 'GET',
+	    path: '/assets/{filename*}',
+	    handler: function(request, reply) {
+	        reply.file('client/assets/' + request.params.filename);
+	    }
+	}, {
+	    method: 'GET',
+	    path: '/modules/{filename*}',
+	    handler: function(request, reply) {
+	        reply.file('client/modules/' + request.params.filename);
+	    }
+	}, {
+	    method: 'GET',
+	    path: '/services/{filename*}',
+	    handler: function(request, reply) {
+	        reply.file('client/services/' + request.params.filename);
+	    }
+	}, {
+	    method: 'GET',
+	    path: '/controllers/{filename*}',
+	    handler: function(request, reply) {
+	        reply.file('client/controllers/' + request.params.filename);
+	    }
+	}]);
+});
 
-		console.log('DB connection established');
+var db = db_module.DBModule(function() {
+    var data = {};
+    var condition = {};
 
-		db.setCollectionName('test_table1');
+    console.log('DB connection established');
 
-		// Data request routes
-		server.route([
-			{
-				method: 'GET',
-				path: '/getAllData',
-				handler: function(request, reply){
-					db.searchDB('ALL', function(data){
-						reply(data);
-		            });
-				}
-			},
-			{
-				method: 'POST',
-				path: '/addName',
-				handler: function(request, reply){
-					data = {
+    db.setCollectionName('test_table1');
+
+    // Data request routes
+	server.route([
+	    {
+	        method: 'GET',
+	        path: '/getAllData',
+	        handler: function(request, reply) {
+	            db.searchDB('ALL', function(data) {
+	                reply(data);
+	            });
+	        }
+	    },
+	    {
+	        method: 'POST',
+	        path: '/addName',
+	        handler: function(request, reply) {
+	            data = {
+	                name: request.payload.name
+	            };
+	            db.insertToDB('ONE', data, function() {
+	                reply();
+	            });
+	        }
+	    },
+	    {
+	        method: 'POST',
+	        path: '/deleteName',
+	        handler: function(request, reply) {
+	            condition = {
+	                _id: db.convertStrToHexStr(request.payload._id)
+	            };
+	            db.deleteFromDB('ONE', condition, function(res) {
+	                reply();
+	            });
+	        }
+	    },
+	    {
+	        method: 'POST',
+	        path: '/saveName',
+	        handler: function(request, reply) {
+	            condition = {
+	                _id: db.convertStrToHexStr(request.payload._id)
+	            };
+	            data = {
+	                $set: {
 	                    name: request.payload.name
-	                };
-	                db.insertToDB('ONE', data, function(){
-	                	reply();
-	                });
-				}
-			},
-			{
-				method: 'POST',
-				path: '/deleteName',
-				handler: function(request, reply){
-					condition = {
-                        _id: db.convertStrToHexStr(request.payload._id)
-                    };
-                    db.deleteFromDB('ONE', condition, function(res){
-                    	reply();
-                    });
-				}
-			},
-			{
-				method: 'POST',
-				path: '/saveName',
-				handler: function(request, reply){
-					condition = {
-                        _id: db.convertStrToHexStr(request.payload._id)
-                    };
-                    data = {
-                        $set: {
-                            name: request.payload.name
-                        }
-                    };
-                    db.updateDB('ONE', condition, data, function(res){
-                    	reply();
-                    });
-				}
-			}
-		]);
-	});
-
-	// Static contents
-	server.route(
-		{
-			method: 'GET',
-			path: '/',
-			handler: function(request, reply){
-				reply.file('client/views/index.html');
-			}
-		},
-		{
-			method: 'GET',
-			path: '/assets/{filename*}',
-			handler: function(request, reply){
-				reply.file('client/assets/' + request.params.filename);
-			}
-		},
-		{
-			method: 'GET',
-			path: '/modules/{filename*}',
-			handler: function(request, reply){
-				reply.file('client/modules/' + request.params.filename);
-			}
-		},
-		{
-			method: 'GET',
-			path: '/services/{filename*}',
-			handler: function(request, reply){
-				reply.file('client/services/' + request.params.filename);
-			}
-		},
-		{
-			method: 'GET',
-			path: '/controllers/{filename*}',
-			handler: function(request, reply){
-				reply.file('client/controllers/' + request.params.filename);
-			}
-		}
-	);
+	                }
+	            };
+	            db.updateDB('ONE', condition, data, function(res) {
+	                reply();
+	            });
+	        }
+	    }
+	]);
 });
 
-server.start(function(err){
-	if(err)
-		throw err;
+server.start(function(err) {
+    if (err)
+        throw err;
 
-	console.log('Server is running at ' + server.info.uri);
+    console.log('Server is running at ' + server.info.uri);
 });
